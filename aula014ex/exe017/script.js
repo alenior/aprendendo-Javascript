40fc2927317db9d3073d5bb2611c8cef44606840
alenior <alenior@gmail.com>
2024-03-22 21:42:26 -0300

    Tabuada

--- aula014ex/exe017/script.js
+++ aula014ex/exe017/script.js
@@ -1,1 +1,18 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.getElementById('inum')
+    let tab = document.getElementById('itab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = '' //Reseta a cada tabuada gerada.
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
